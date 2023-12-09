6f3d1972bc66a73a2bdd6576bbc56cc69140c827
bara <baralul01@gmail.com>
2023-12-09 23:38:47 +0700

    added option to display data from each or both gender

--- statsNprop/gendervshourpiechart.py
+++ statsNprop/gendervshourpiechart.py
@@ -4,28 +4,40 @@ from matplotlib import pyplot as plt
 
 def genderhvshourpiechart(whichactivity, whichgender):
     data = pd.read_csv('data1komputer.csv')
-    male = []
-    female = []
-    both = []
-    for index, row in data.iterrows():
-        both.append(row)
-        if row['gender'] == "Laki-laki":
-            male.append(row)
-        elif row['gender'] == "Perempuan":
-            female.append(row)
-
     appscount = {}
+    appscountmale = {}
+    appscountfemale = {}
     for index, row in data.iterrows():
         usage = row['usage']
         if usage in appscount:
             appscount[usage] += 1
         else:
             appscount[usage] = 1
-    labels = list(appscount.keys())
-    slices = list(appscount.values())
+        if row['gender'] == "Laki-laki":
+            if usage in appscountmale:
+                appscountmale[usage] += 1
+            else:
+                appscountmale[usage] = 1
+        elif row['gender'] == "Perempuan":
+            if usage in appscountfemale:
+                appscountfemale[usage] += 1
+            else:
+                appscountfemale[usage] = 1
+    labels = []
+    slices = []
+    if whichgender == "Laki-laki":
+        labels = list(appscountmale.keys())
+        slices = list(appscountmale.values())
+    elif whichgender == "Perempuan":
+        labels = list(appscountfemale.keys())
+        slices = list(appscountfemale.values())
+    elif whichgender == "Laki-laki dan Perempuan":
+        labels = list(appscount.keys())
+        slices = list(appscount.values())
     plt.pie(slices, labels=labels, autopct='%1.1f%%', wedgeprops={'edgecolor': 'black'})
     plt.title(f"Rata-rata jumlah jam dihabiskan untuk {whichactivity} responden {whichgender}")
     plt.tight_layout()
     plt.show()
+
 
 genderhvshourpiechart('komputer', 'Perempuan')
