583935065b57f51466738c0fed7710f92d70a842
bara <baralul01@gmail.com>
2024-03-20 10:01:32 +0700

    added notes under 'pertemuan05'
    struct, class, constructor, setter & getter

---
+++ structuredprogramming/pertemuan05/class3.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+
+class Mahasiswa{
+	public:
+		string nama, npm;
+		
+		Mahasiswa(){
+			this->nama = nama;
+			this->npm = b;
+		}
+};
+
+int main(){
+	Mahasiswa Mhs("Reza","2117");
+	
+	cout << "Nama: " << Mhs.nama << endl;
+	cout << "Npm: " << Mhs.npm << endl;
+}
---
+++ structuredprogramming/pertemuan05/class4.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std;
+
+class Mahasiswa{
+	private:
+		string nama;
+	
+	public:
+		void setNama(string nama) {
+			this->nama = nama;
+		}
+		
+		string getNama() {
+			return nama;
+		}
+};
+
+int main(){
+	Mahasiswa Mhs;
+	
+	mhs.setNama("Anta");
+	
+	cout << "Nama: " << Mhs.getNama() << endl;
+}
