0b49e929a82d7ee4f05809a1c75faf22355f23b4
bara <baralul01@gmail.com>
2024-05-05 01:49:32 +0700

    added snuke.cpp
    Problem Statement
    
    There is a grid with H horizontal rows and W vertical columns. Each cell has a lowercase English letter written on it. We denote by (i,j) the cell at the i-th row from the top and j-th column from the left.
    
    The letters written on the grid are represented by H strings S1,S2,…,SH, each of length W. The j-th letter of Si represents the letter written on (i,j).
    
    There is a unique set of contiguous cells (going vertically, horizontally, or diagonally) in the grid with s, n, u, k, and e written on them in this order.
    Find the positions of such cells and print them in the format specified in the Output section.
    
    A tuple of five cells (A1,A2,A3,A4,A5) is said to form a set of contiguous cells (going vertically, horizontally, or diagonally) with s, n, u, k, and e written on them in this order if and only if all of the following conditions are satisfied.
    
        A1,A2,A3,A4 and A5 have letters s, n, u, k, and e written on them, respectively.
        For all 1≤i≤4, cells Ai and Ai+1 share a corner or a side.
        The centers of A1,A2,A3,A4, and A5 are on a common line at regular intervals.
    
    Constraints
    
        5≤H≤100
        5≤W≤100
        H and W are integers.
        Si is a string of length W consisting of lowercase English letters.
        The given grid has a unique conforming set of cells.
    
    Input
    
    The input is given from Standard Input in the following format:
    
    H W
    S1
    S2
    ⋮
    SH
    
    Output
    
    Print five lines in the following format.
    
    Let (R1,C1),(R2,C2)…,(R5,C5) be the cells in the sought set with s, n, u, k, and e written on them, respectively. The i-th line should contain Ri and Ci in this order, separated by a space.
    
    In other words, print them in the following format:
    
    R1 C1
    R2 C2
    ⋮
    R5 C5
    [B
    
    I used the code for the findwords assignment and figured out that I can use it again for this problem with a little bit of adjustment because why not

---
+++ structuredprogramming/utp/snuke.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+
+using namespace std;
+
+
+bool searchHorizontal(char matrix[105][105], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            string diagStrR = "";
+            for (int k = 0; k < 5; k++) {
+                if (j + k < cols) {
+                    diagStrR += matrix[i][j + k];
+                }
+            }
+            if (diagStrR == "snuke") {
+                for (int k = 0; k < 5; k++) {
+                    cout << i + 1 << " " << j + k + 1 << endl;
+                }
+                return true;
+            }
+        }
+    }
+    return false; 
+}
+
+
+bool searchVertical(char matrix[105][105], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            string diagStrR = "";
+            for (int k = 0; k < 5; k++) {
+                if (i + k < rows) {
+                    diagStrR += matrix[i + k][j];
+                }
+            }
+            if (diagStrR == "snuke") {
+                for (int k = 0; k < 5; k++) {
+                    cout << i + k + 1 << " " << j + 1 << endl;
+                }
+                return true;
+            }
+        }
+    }
+    return false; 
+}
+
+bool searchDiagonalRight(char matrix[105][105], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            string diagStrR = "";
+            for (int k = 0; k < 5; k++) {
+                if (i + k < rows && j + k < cols) {
+                    diagStrR += matrix[i + k][j + k];
+                }
+            }
+            if (diagStrR == "snuke") {
+                for (int k = 0; k < 5; k++) {
+                    cout << i + k + 1 << " " << j + k + 1 << endl;
+                }
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool searchDiagonalLeft(char matrix[105][105], int rows, int cols) {
+    for (int i=0; i<rows; i++) {
+        for (int j=cols-1; j>=0; j--) {
+            string diagStrL = "";
+            for (int k = 0; k < 5; k++) {
+                if (i + k < rows && j - k >= 0) {
+                    diagStrL += matrix[i - k][j - k];
+                }
+            }
+            if (diagStrL == "snuke") {
+                for (int k = 0; k < 5; k++) {
+                    cout << i - k+1 << " " << j - k+1 << endl;
+                }
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool searchDiagonalRightUp(char matrix[105][105], int rows, int cols) {
+    for (int i=rows-1; i>=0; i--) {
+        for (int j=0; j<cols; j++) {
+            string diagStrRU = "";
+            for (int k=0; k<5; k++) {
+                if (i - k >= 0 && j + k < cols) {
+                    diagStrRU += matrix[i - k][j + k];
+                }
+            }
+            if (diagStrRU == "snuke") {
+                for (int k=0; k<5; k++) {
+                    cout << i - k+1 << " " << j + k+1 << endl;
+                }
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool searchDiagonalLeftUp(char matrix[105][105], int rows, int cols) {
+    for (int i=rows-1; i>=0; i--) {
+        for (int j=cols-1; j>=0; j--) {
+            string diagStrLU = "";
+            for (int k=0; k<5; k++) {
+                if (i - k >= 0 && j - k >= 0) {
+                    diagStrLU += matrix[i - k][j - k];
+                }
+            }
+            if (diagStrLU == "snuke") {
+                for (int k=0; k<5; k++) {
+                    cout << i - k+1 << " " << j - k+1 << endl;
+                }
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void searchWordsInMatrix(char matrix[105][105], int rows, int cols) {
+    searchHorizontal(matrix, rows, cols);
+    searchVertical(matrix, rows, cols);
+    searchDiagonalRight(matrix, rows, cols);
+    searchDiagonalLeft(matrix, rows, cols);
+    searchDiagonalRightUp(matrix, rows, cols);
+    searchDiagonalLeftUp(matrix, rows, cols);
+}
+
+
+int main() {
+    int H, W;
+    
+    cin >> H >> W;
+    cin.ignore();
+
+    char matrix[105][105];
+
+    for (int i = 0; i < H; ++i) {
+        cin.getline(matrix[i], W+1); 
+    }
+
+    searchWordsInMatrix(matrix, H, W);
+
+    return 0;
+}
+
