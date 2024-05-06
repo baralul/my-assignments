94b9b3073814023285fdaa579a4b930d5dcb3c24
bara <baralul01@gmail.com>
2024-05-06 20:22:56 +0700

    added soal1.cpp
    Anda akan diberikan array N bilangan bulat dan Anda diminta mencetak bilangan bulat yang bernilai ganjil.
    
    Format Input
    
    Baris pertama input berisi N, di mana N adalah banyaknya bilangan bulat. Baris berikutnya berisi N bilangan bulat yang dipisahkan spasi.
    
    Format Output
    
    Cetak bilangan bulat dari array yang bernilai ganjil, dipisahkan oleh spasi dalam satu baris.
    
    Constraint
    
    1 < N < 1000

---
+++ structuredprogramming/stackNqueue/soal1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	int arr[N];
+	
+	for(int i=0; i<N; i++) {
+	    cin >> arr[i];
+	}
+	for(int i=0; i<N; i++) {
+	    if(arr[i]%2 == 1) {
+	        cout << arr[i] << " ";
+	    }
+	}
+	
+    return 0;
+}
