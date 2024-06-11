d696ba7b795aefc058a33101cbb5474b3d12ed96
bara <106955611+baralul@users.noreply.github.com>
2024-06-11 15:48:25 +0700

    added enter to proceed, exit confirmation, improved readability & made the CLI prettier

--- structuredprogramming/UAP.cpp
+++ structuredprogramming/UAP.cpp
@@ -14,6 +14,7 @@ class User;
 int selectCake();
 int selectFlavor();
 int selectSize();
+double calculateTotal(Cake array[], int n);
 Cake listingCake();
 void orderCake();
 void confirmOrder(Cake array[], int n);
@@ -22,6 +23,7 @@ void showCatalog();
 void itemList();
 void showReceipt(Cake array[], int n);
 void registerUser();
+void showTime();
 bool loginUser();
 User* findUser(const string& username);
 
@@ -89,10 +91,8 @@ class Cake {
 void mainMenu() {
     
     while(in_loop) {
-        
        int navigate = -1;
-        cout << "Welcome to Hammer Bakery! How can we help you?" 
-            << "\n1. Login\n2. Register\n3. Exit" << endl;
+            cout << "1. Login\n2. Register\n3. Exit" << endl;
         
         while(!(navigate >= 1 && navigate <= 3)) {
             cout << "Please select from the available option: ";
@@ -108,6 +108,7 @@ void mainMenu() {
                 is_logged_in = true;
                 while (is_logged_in) {
                     int action = -1;
+                    cout << "Welcome to Hammer Bakery! How can we help you?" << endl;
                     cout << "1. Order\n2. Show menu\n3. Logout" << endl;
                     
                     while (!(action >= 1 && action <= 3)) {
@@ -120,12 +121,18 @@ void mainMenu() {
                         orderCake();
                     } else if (action == 2) {
                         showCatalog(); 
-                        cout << setw(50) << left << "Press enter to return" << endl; // todo: press enter to return to main menu
-                        cin.clear();
+                        cout << "Press enter to return";
+                        cin.ignore(1000, '\n');
                         cin.get(); 
+                        system("clear");
                     } else if (action == 3) {
+                        cout << "Logging out." << endl;
+                        sleep(1);
+                        system("clear");
+                        cout << "Logging out.." << endl;
+                        sleep(1);
                         is_logged_in = false;
-                        cout << "Logged out.\n" << endl;
+                        system("clear");
                     }
                 }
             } else {
@@ -136,7 +143,22 @@ void mainMenu() {
         } else if (navigate == 2) {
             registerUser();
         } else if (navigate == 3) {
-            in_loop = false;
+            int really;
+            while(really != 1 || really != 2){
+                cout << "are you sure want to exit?\n1. Yes\n2. No\nPlease select from the available option: ";
+                cin >> really;
+                system("clear");
+                if(really == 1){
+                    in_loop = false;
+                    system("clear");
+                    cout << "See you next time!" << endl;
+                    break;
+                }
+                else if(really == 2){
+                    system("clear");
+                    break;
+                }
+            }
         }
     }
 }
@@ -270,14 +292,19 @@ void showCatalog() {
 }
 
 void itemList(Cake array[], int n) {
-    cout << setw(15) << left << "Flavor" << setw(15) << "Cake" << setw(15) 
-        << "Size" << setw(5) << "Price" << endl;
-    for(int i=0; i<n; i++) {
-        cout << setw(15) << left << array[i].getFlavor() 
-            << setw(15) << array[i].getName() 
-            << setw(15) << array[i].getSize()
-            << setw(5) << array[i].getPrice() << endl;
+    cout << "------------------------------------------------------------" << endl;
+    cout << "| " << setw(15) << left << "Flavor" 
+         << "| " << setw(15) << "Cake" 
+         << "| " << setw(15) << "Size" 
+         << "| " << setw(5) << "Price" << " |" << endl;
+    cout << "------------------------------------------------------------" << endl;
+    for(int i = 0; i < n; i++) {
+        cout << "| " << setw(15) << left << array[i].getFlavor() 
+             << "| " << setw(15) << array[i].getName() 
+             << "| " << setw(15) << array[i].getSize() 
+             << "| " << setw(5) << array[i].getPrice() << " |" << endl;
     }
+    cout << "------------------------------------------------------------" << endl;
 }
 
 void confirmOrder(Cake array[], int n) {
@@ -285,7 +312,7 @@ void confirmOrder(Cake array[], int n) {
     
     cout << "Here's your order list: " << endl;
     itemList(array, n);
-    cout << "Proceed with the order?\n1. Yes\n2. No" << endl;
+    cout << "Proceed with the order?\n1. Yes\n2. No\nPlease select from the available option: ";
     cin >> answer;
     system("clear");
     
@@ -294,7 +321,7 @@ void confirmOrder(Cake array[], int n) {
     }
 }
 
-void showReceipt(Cake array[], int n) {
+void showTime(){
     time_t now = time(0);
     tm *gmtm = gmtime(&now);
     gmtm->tm_hour += 7;  
@@ -302,18 +329,30 @@ void showReceipt(Cake array[], int n) {
     char dt[30];
     strftime(dt, sizeof(dt), "%Y-%m-%d %H:%M:%S WIB", gmtm); 
     cout << setw(50) << left << dt << endl;
+}
 
-    itemList(array, n);
-    
+double calculateTotal(Cake array[], int n){
     double total = 0;
     for (int i = 0; i < n; i++) {
         total += array[i].getPrice();
     }
+    return total;
+}
+
+void showReceipt(Cake array[], int n) {
+    double total;
     
-    cout << setw(45) << left << "Total" << total << endl;
+    showTime();
+    itemList(array, n);
+    total = calculateTotal(array, n);
+    
+    cout << "| " << setw(51) << left << "Total" << setw(5) << total << " |" << endl;
+    cout << "------------------------------------------------------------" << endl;
     cout << setw(50) << left << "Thank you for purchasing in our shop!" << endl;
-    cout << setw(50) << left << "Press enter to continue" << endl;
-    cin.get();
+    cout << "Press enter to continue";
+    cin.ignore(1000, '\n');
+    cin.get(); 
+    system("clear");
 }
 
 int main() {
